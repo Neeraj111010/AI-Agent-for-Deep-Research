305f0c8a24681a8d95e0697ce413de2106c65f38
Neeraj111010 <136294499+Neeraj111010@users.noreply.github.com>
2025-07-05 15:07:20 +0530

    Update services.ts

--- src/app/api/deep-research/services.ts
+++ src/app/api/deep-research/services.ts
@@ -1,6 +1,4 @@
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
-import {z} from 'zod'
-
 import Exa from "exa-js"
 
 export const exa = new Exa(process.env.EXA_SEARCH_API_KEY || "");
